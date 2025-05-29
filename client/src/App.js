ece49e9e2af2bbdfbf515396064f2e673b469cc5
MaheshNagaladinne <nagaladinnemahesh07@gmail.com>
2025-05-29 23:51:20 +0530

    Add SignupPage UI and logic - v0.6-signup-page

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,19 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './pages/Login';
+import LoginPage from './pages/LoginPage';
 import Dashboard from './pages/Dashboard';
+import SignupPage from './pages/SignupPage';
+import HomePage from './pages/HomePage';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <Routes>
-        <Route path="/login" element={<Login />} />
+        <Route path='/homepage' element={<HomePage />} />
+        <Route path='/signup' element={<SignupPage />} />
+        <Route path="/login" element={<LoginPage />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/" element={<h1>Welcome to Smart Career Counselor App</h1>} />
       </Routes>
     </Router>
   );
